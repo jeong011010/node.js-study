ba7755091026ec2eb6669bf2b6bde53ccc523be9
정훈 김 <jeong01101095@gmail.com>
2022-04-09 16:13:43 +0900

    study JS about [template literal]

---
+++ 01/syntex/template.js
@@ -0,0 +1,10 @@
+var name = 'k8805';
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ${name}`;
+console.log(letter);
+// template literal
+// '`' 기호로 시작과 끝을 맺어 template literal을 정의
+// 해당 기호 안에 있는 엔터와 같은 변화를 전부 적용.
+// 변수를 넣을 때는 ${} 안에 대입
+// \n, +, '' 와 같은 기호 대신하여 편리하게 사용할 수 있도록 만든 템플릿
