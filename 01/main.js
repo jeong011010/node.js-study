5848710ad717c96fe96e893247df7b5a23d73c7e
정훈 김 <jeong01101095@gmail.com>
2022-04-10 22:10:13 +0900

    study js about [post file, redirection]

--- 01/main.js
+++ 01/main.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
+var qs = require('querystring'); 
+
 function templateHTML(title, list, body){
   return `
   <!doctype html>
@@ -13,6 +14,9 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+
+    <a href="/create">create</a>
+
     ${body}
   </body>
   </html>
@@ -54,12 +58,46 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - Create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process"
+        method="post">
+            <p><input type="text" name = "title" placeholder="title"></p>
+            <p>
+                <textarea name="description"placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname === '/create_process'){
+      var body='';
+      request.on('data', function(data){
+        body += data;
+        if(body.length>1e6)
+          request.connection.destroy();
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`,description,'utf8',function(err){
+          response.writeHead(302,
+              {Location: `/?id=${title}`}
+            );
+          response.end();
+        })
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
- 
- 
- 
 });
 app.listen(3000);
