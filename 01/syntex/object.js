9208d14acf4bc3e4d880e8b6274187285dd884a3
정훈 김 <jeong01101095@gmail.com>
2022-04-12 05:00:32 +0900

    study js about [function of object : 값으로서 함수 -> 배열, 객체에 대입 가능한 함수]

---
+++ 01/syntex/object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]);
+
+var roles = {
+    'programmer':'eoging',
+    'designer' :'k8805',
+    'manager' : 'hoya'
+}
+console.log(roles.designer);
