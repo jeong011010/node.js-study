8d867c5b04dd3c5e57d58541c079b001675bb682
정훈 김 <jeong01101095@gmail.com>
2022-04-12 05:03:45 +0900

    study js about [object hash, map]

--- 01/syntex/object.js
+++ 01/syntex/object.js
@@ -6,4 +6,8 @@ var roles = {
     'designer' :'k8805',
     'manager' : 'hoya'
 }
+// 객체는 key 값과 value로 이루어짐
+// 값을 찾을 때 정해둔 key값으로 바로 찾을 수 있음
+// 순서가 없으며 각자의 객체대로 값이 저장됨.
+// c++의 해시테이블, map 보는 느낌
 console.log(roles.designer);
