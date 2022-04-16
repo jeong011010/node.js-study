add8eff60995ef4ab2974dfd4690e22e2574047f
정훈 김 <jeong01101095@gmail.com>
2022-04-16 13:39:12 +0900

    use mixin (function of reused html)

--- youtubeclone/controllers/videoController.js
+++ youtubeclone/controllers/videoController.js
@@ -1,8 +1,16 @@
-export const home = (req, res) => res.render('home');
+import { videos } from "../db";
+
 export const search = (req, res) => {
     const searchid = req.query.id;
-    res.render("search",{
-       pageTitle:"Search",
-       searchid: searchid 
-    });
+      res.render("search", {
+        pageTitle: "Search",
+        searchid,
+        videos
+      });
 };
+export const home = (req, res) => {
+    res.render("home", {
+        pageTitle:"Main",
+        videos:videos
+    });
+}
---
+++ youtubeclone/db.js
@@ -0,0 +1,12 @@
+export const videos = [{
+    id: 1881,
+    title: 'A Study in Scarlet',
+    description: "I should like to meet him.",
+    views: 28,
+    videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+    creator: {
+      id: 12345,
+      name: "Nana",
+      email: "nykim@nykim.net",
+    }
+}]
